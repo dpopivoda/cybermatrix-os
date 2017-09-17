0f4411b740ffca4f2e63a823dfbcf709b35b84c4
dpopivoda <skrapicverse@gmail.com>
2017-09-17 21:28:56 +0200

    Added SFML submodule and updated CMakeLists.txt to support it

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main(int argc, char** argv) {
-	std::cout << "Hello world!" << std::endl;
+	sf::RenderWindow window(sf::VideoMode(200, 200), "Cybermatrix OS");
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+		window.clear();
+		window.display();
+    }
 	return 0;
 }
